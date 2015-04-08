4e151ae576dbe7dbc2f7d9551a4a92dbb1c60eaf
ciberkleid <ciberkleid@pivotal.io>
2015-04-08 11:12:32 -0400

    Green deploy, with env variables

--- src/main/java/org/example/cities/StagingController.java
+++ src/main/java/org/example/cities/StagingController.java
@@ -1,5 +1,7 @@
 package org.example.cities;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,19 @@ public class StagingController {
 		String green = "#9FF781";
 		
 		// Blue/Green deploy demo
-		String color = blue;
-		// String color = green;
-		return "<body style=\"background:" + color + "\"><h1><center>Hello, World!</center></h1>"; // blue
+		// String color = blue;
+		String color = green;
+		
+		StringBuffer s = new StringBuffer();
+		
+		s.append("<body style=\"background:");
+		s.append(color);
+		s.append("\"><h1><center>Hello, World!</center></h1>");
+		s.append("<br><br>Application Environment Variables:<br>");
+		for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
+	        s.append("<br><b>" + envvar.getKey() + ":</b> " + envvar.getValue());
+	    }
+		return s.toString();
+		
 	}
 }
