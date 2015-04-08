290b4383aa56d002da583a55e5676392ede0d89d
ciberkleid <ciberkleid@pivotal.io>
2015-04-08 13:15:17 -0400

    Blue with logging

--- src/main/java/org/example/cities/StagingController.java
+++ src/main/java/org/example/cities/StagingController.java
@@ -10,40 +10,41 @@ public class StagingController {
 
 	@RequestMapping("/")
 	public String index() {
-		
+
+		System.out
+				.println("Generating home page for app at " + System.getenv().get("VCAP_APP_HOST")
+						+ ":" + System.getenv().get("VCAP_APP_PORT"));
+
 		String blue = "#81BEF7";
 		String green = "#9FF781";
-		
+
 		// Blue/Green deploy demo
-		// String color = blue;
-		String color = green;
-		
+		String color = blue;
+		// String color = green;
+
 		StringBuffer s = new StringBuffer();
-		
+
 		s.append("<body style=\"background:");
 		s.append(color);
 		s.append("\">");
 		s.append("<h1><center>Hello, World!</center></center></h1>");
 		s.append("<center>");
 		s.append("<br>This app is running on ");
-		s.append(System.getenv().get("VCAP_APP_HOST") + ":" + System.getenv().get("VCAP_APP_PORT") );
+		s.append(System.getenv().get("VCAP_APP_HOST") + ":" + System.getenv().get("VCAP_APP_PORT"));
 		s.append("</center>");
-		
+
 		/*
-		s.append("<table border=\"1\" style=\"width:60%\">");
-		s.append("<tr>");
-		s.append("<th colspan=\"2\">Application Environment Variables</th>");
-		s.append("</tr>");
-		for (Map.Entry<String, String> envvar : System.getenv().entrySet()) {
-			s.append("<tr>");
-			s.append("<td>"+envvar.getKey()+"</td>");
-			s.append("<td>"+envvar.getValue()+"</td>");
-			s.append("</tr>");
-	    }
-		s.append("</table>");
-		*/
-		
+		 * s.append("<table border=\"1\" style=\"width:60%\">");
+		 * s.append("<tr>");
+		 * s.append("<th colspan=\"2\">Application Environment Variables</th>");
+		 * s.append("</tr>"); for (Map.Entry<String, String> envvar :
+		 * System.getenv().entrySet()) { s.append("<tr>");
+		 * s.append("<td>"+envvar.getKey()+"</td>");
+		 * s.append("<td>"+envvar.getValue()+"</td>"); s.append("</tr>"); }
+		 * s.append("</table>");
+		 */
+
 		return s.toString();
-		
+
 	}
 }
